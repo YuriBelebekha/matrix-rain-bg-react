0d2ccc6891e41527fa4a2efb25071f52e598b7ca
Yuri Belebekha <ybelebekha@gmail.com>
2023-03-28 20:15:05 +0300

    Added matrix font

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.Container}>
+      ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\|{}<>[]^~</>
     </div>
   );
 };
